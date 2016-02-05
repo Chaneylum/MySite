72eee6016eb6edefaf55851210b904186c12f55e
Apostic <Chaneylum@ymail.com>
2016-02-05 11:08:42 -0500

    Just doing some clean-up and content editing while I explore the code.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 (function () {
 
-	var content = "Suspendisse vel volutpat elit. Sed ultrices posuere elit, quis placerat enim. Vivamus at sem sapien. Morbi magna est, porttitor non tellus eu, dignissim dictum urna. Suspendisse nec placerat sem. Aliquam dapibus, erat at sodales dignissim, lectus mauris fringilla felis, eget ullamcorper nisl quam eget felis. Nam quis vulputate ex. Aliquam sit amet quam ut lectus accumsan vestibulum non id nibh. Praesent eget laoreet tellus, quis gravida enim. Sed porttitor condimentum elit, ut ultrices augue suscipit a. Donec scelerisque nisl ut aliquam pulvinar. Ut accumsan mi efficitur turpis congue fringilla.";
+	var content = "Suspendisse vel volutpat elit.";
 
 	var contentElem;
 
